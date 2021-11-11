1c44b60ddc603213c035c2b7c539f1e3269e73ce
eliran <scherzter@gmail.com>
2021-11-11 12:58:11 -0500

    changing the time request for Beluga.

--- code/sbatch_basic.sh
+++ code/sbatch_basic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -t 0-12:00
+#SBATCH -t 0-4:00
 #SBATCH -A def-dkrass
 source /home/eliransc/.virtualenvs/deep_queue/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/sampling_ph_1.py
