c2d6b783c41b2991ecdff9d7a3011b08bd8d5700
eliran <scherzter@gmail.com>
2021-11-11 09:57:52 -0500

    Setting 1000 examples

--- code/sampling_ph_1.py
+++ code/sampling_ph_1.py
@@ -887,7 +887,7 @@ def main(args):
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_type', type=str, help='mixture erlang or general', default='Gen_ph')
-    parser.add_argument('--num_examples', type=int, help='number of ph folders', default=12)
+    parser.add_argument('--num_examples', type=int, help='number of ph folders', default=1000)
     parser.add_argument('--max_num_groups', type=int, help='mixture erlang or general', default=2)
     parser.add_argument('--num_moms', type=int, help='number of ph folders', default=40)
     parser.add_argument('--batch_size', type=int, help='number of ph examples in one folder', default=128)
