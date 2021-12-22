4c873ffd1c9a84bdada77a574d58f8e3bdc4c250
eliran <scherzter@gmail.com>
2021-12-22 10:40:03 -0500

    adding train_cpu.py

--- code/sbatch_train_m_ph_1.sh
+++ code/sbatch_train_m_ph_1.sh
@@ -2,7 +2,7 @@
 #SBATCH -t 0-05:00
 #SBATCH -A def-dkrass
 #SBATCH --gpus-per-node=1
-#SBATCH --mem-per-cpu=3500M
+#SBATCH --mem-per-cpu=9000M
 source /home/eliransc/.virtualenvs/deep_queue/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/train_cpu.py
 
