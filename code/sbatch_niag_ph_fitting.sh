88a71f96aad905ea8e9e5b2d8995762e68c2767e
eliransc <scherzter@gmail.com>
2022-01-19 19:36:10 -0500

    first commit

--- code/sbatch_niag_ph_fitting.sh
+++ code/sbatch_niag_ph_fitting.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #SBATCH -t 0-18:58
 #SBATCH -A def-dkrass
-#SBATCH --mem 10000
 source /home/d/dkrass/eliransc/queues/bin/activate
 python /home/d/dkrass/eliransc/Deep_queue/code/fitting_ph.py
