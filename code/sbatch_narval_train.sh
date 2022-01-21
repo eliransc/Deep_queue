7590b13bc4d70c07edcf6d04f07d06d2c6eea22f
eliransc <scherzter@gmail.com>
2022-01-21 15:45:58 -0500

    first commit

--- code/sbatch_narval_train.sh
+++ code/sbatch_narval_train.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH -t 0-20:58
 #SBATCH -A def-dkrass
-#SBATCH --mem 10000
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-cpu=10000M
 source /home/eliransc/projects/def-dkrass/eliransc/virenv/queues/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/train_cpu.py
