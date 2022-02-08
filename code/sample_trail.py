549550b2b19b1c7b3de78fb4cd65d4a6464e34cc
eliransc <scherzter@gmail.com>
2022-02-08 15:54:59 -0500

    samnple_trails

--- code/sample_trail.py
+++ code/sample_trail.py
@@ -731,7 +731,11 @@ def main(args):
 
     if sys.platform == 'linux':
 
-        data_path = '/scratch/d/dkrass/eliransc/training/gg1'
+        if os.getcwd() =='/gpfs/fs0/scratch/d/dkrass/eliransc/Deep_queue/code':
+
+            data_path = '/scratch/d/dkrass/eliransc/training/gg1'
+        else:
+            data_path = '/scratch/training/gg1'
 
     else:
 
