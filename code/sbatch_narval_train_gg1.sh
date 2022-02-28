f05d048dffecab027805e87b8869e1c3b5687921
eliransc <scherzter@gmail.com>
2022-02-27 20:58:20 -0500

    samnple_trails

---
+++ code/sbatch_narval_train_gg1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -t 0-23:58
+#SBATCH -A def-dkrass
+#SBATCH --gpus-per-node=1
+#SBATCH --mem-per-cpu=10000M
+source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
+python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/train_GG1.py
