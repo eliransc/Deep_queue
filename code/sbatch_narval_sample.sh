911932df64276348f7da6bbe9e612bc98326fc0c
eliransc <scherzter@gmail.com>
2022-03-29 11:59:24 -0400

    samnple_trails

--- code/sbatch_narval_sample.sh
+++ code/sbatch_narval_sample.sh
@@ -2,5 +2,5 @@
 #SBATCH -t 0-20:58
 #SBATCH -A def-dkrass
 #SBATCH --mem 10000
-source /home/eliransc/projects/def-dkrass/eliransc/queues/activate
+source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/sampling_ph_1.py
