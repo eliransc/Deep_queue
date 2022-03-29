c71eb9d9691aedcaac887b85712a574a5c1b41d4
eliransc <scherzter@gmail.com>
2022-03-29 14:09:56 -0400

    train_cpu.py with erlang data

--- code/sbatch_narval_train.sh
+++ code/sbatch_narval_train.sh
@@ -3,5 +3,5 @@
 #SBATCH -A def-dkrass
 #SBATCH --gpus-per-node=1
 #SBATCH --mem-per-cpu=10000M
-source /home/eliransc/projects/def-dkrass/eliransc/virenv/queues/bin/activate
+source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/train_cpu.py
