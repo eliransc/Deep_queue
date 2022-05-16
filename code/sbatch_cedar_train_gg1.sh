4d7cb29172e0191c84c096904616da2b4c93a548
eliransc <scherzter@gmail.com>
2022-05-16 10:06:25 -0400

    train_cpu.py with erlang data

--- code/sbatch_cedar_train_gg1.sh
+++ code/sbatch_cedar_train_gg1.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH -t 0-20:58
+#SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
 #SBATCH --mem 10000
+#SBATCH --gpus-per-node=1
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/sampling_ph_1.py
+python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/train_GG1.py
 
-source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/sample_trail.py
