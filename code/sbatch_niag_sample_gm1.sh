b377e8fbb4b68cbd6c3f0e94fcf0689402bacdee
eliransc <scherzter@gmail.com>
2022-05-26 10:52:36 -0400

    train_cpu.py with erlang data

--- code/sbatch_niag_sample_gm1.sh
+++ code/sbatch_niag_sample_gm1.sh
@@ -2,4 +2,4 @@
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
 source /home/d/dkrass/eliransc/queues/bin/activate
-python /scratch/d/dkrass/eliransc/Deep_queue/code/sample_g_m_1.py
+python /scratch/d/dkrass/eliransc/Deep_queue/code/sample_trail.py
