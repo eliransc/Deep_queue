403f8e619560646cf8e94fcaadf39bc8e17383a8
eliransc <scherzter@gmail.com>
2022-06-03 08:19:58 -0400

    train_cpu.py with erlang data

--- code/train_GG1.py
+++ code/train_GG1.py
@@ -827,7 +827,7 @@ def check_loss_increasing(loss_list, n_last_steps=10, failure_rate=0.45):
 
 def main():
 
-    batch_size = 128
+    batch_size = 256
     now = datetime.now()
     print('Start training')
     current_time = now.strftime("%H_%M_%S") + '_' + str(np.random.randint(1, 1000000, 1)[0])
