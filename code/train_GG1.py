8517a6642f4890c401069f5203c3b1c013f19b46
eliransc <scherzter@gmail.com>
2022-06-06 21:28:19 -0400

    train_cpu.py with erlang data

--- code/train_GG1.py
+++ code/train_GG1.py
@@ -974,10 +974,10 @@ def main():
                                                                                                                     compute_sum_error_list[
                                                                                                                         -1],
                                                                                                                     time.time() - t_0))
-            torch.save(net.state_dict(), '../gg1_models/pytorch_g_g_1_true_moms_new_data_' + str(num_moms) + '_moms_2M_data'+ 'new_archi' + str(
+            torch.save(net.state_dict(), '../gg1_models/pytorch_g_g_1_true_moms_new_data_' + str(num_moms) + '_moms_2M_data'+ 'new_archi'+'loss_1' + str(
                 current_time) + '.pkl')
             pkl.dump((loss_list, valid_list, compute_sum_error_list),
-                     open('../gg1_models/losts_' + str(num_moms) + '_moms_2M_data'+ 'new_archi'+ 'batch_size'+ str(batch_size)+ '_' + str(current_time) + '.pkl', 'wb'))
+                     open('../gg1_models/losts_' + str(num_moms) + '_moms_2M_data'+ 'new_archi'+'loss_1' + 'batch_size'+ str(batch_size)+ '_' + str(current_time) + '.pkl', 'wb'))
 
 if __name__ == "__main__":
 
