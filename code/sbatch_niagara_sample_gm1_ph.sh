4711e7fe93c99678749577321cea2e02d8067a86
eliransc <scherzter@gmail.com>
2022-06-22 15:17:35 -0400

    Lindlely gg1 queue

--- code/sbatch_niagara_sample_gm1_ph.sh
+++ code/sbatch_niagara_sample_gm1_ph.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -t 0-19:58
 #SBATCH -A def-dkrass
-source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
+source /home/d/dkrass/eliransc/queues/bin/activate
 python /scratch/d/dkrass/eliransc/Deep_queue/code/sample_g_m_1.py
 
 
